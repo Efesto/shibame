39e9ab8df28f9078083ed7ef3589e9057c282ae4
Marco Polita <polita.marco@gmail.com>
2018-03-17 12:48:15 +0100

    Adds Heroku keep awake process

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+require('./keep_awake');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
   response.send('Hello from Express!')
