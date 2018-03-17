d02ccb7f6bd19be3679ca941cec74dafa73bcb13
Marco Polita <polita.marco@gmail.com>
2018-03-17 12:42:45 +0100

    first commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('[ERROR] ', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
