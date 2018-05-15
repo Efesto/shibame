f090381b76241a697bd7ebc8e47a1a91207833ed
Marco Polita <polita.marco@gmail.com>
2018-05-15 17:43:59 +0200

    adds dotenv, adds many shibas route

--- app/shibas.js
+++ app/shibas.js
@@ -1,28 +1,62 @@
+require('dotenv').load();
 const tumblr = require('tumblr.js');
 const tumblr_consume_key = process.env.TUMBLR_CONSUMER_KEY;
 const client = tumblr.createClient(
   { consumer_key: tumblr_consume_key }
 );
 
+const getPosts = new Promise(((resolve, reject) => {
+  const blogs = [
+    'shibalovers.tumblr.com',
+    'shibasommelier.tumblr.com',
+    'mensweardog.tumblr.com'
+  ];
+
+  client.blogPosts(
+    blogs.random(),
+    { type: 'photo', limit: 500 },
+    (err, data) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    }
+  );
+}));
+
 const register = (app) => {
   app.get('/random', (request, response) => {
-    const blogs = [
-      'shibalovers.tumblr.com',
-      'shibasommelier.tumblr.com',
-      'mensweardog.tumblr.com'
-    ];
-    client.blogPosts(
-      blogs.random(),
-      { type: 'photo', limit: 500 },
-      (err, data) => {
-        const post = data.posts.random();
-        const shiba = post.photos.random();
-        response.json(
-          {
-            shiba: shiba.alt_sizes[0].url
-          }
-        );
-      });
+    getPosts.then(data => {
+      const post = data.posts.random();
+      const shiba = post.photos.random();
+      response.json(
+        {
+          shiba: shiba.alt_sizes[0].url
+        }
+      );
+    }).catch(() => {
+      response.json({
+        error: 'No doggos here'
+      })
+    });
+  })
+
+  app.get('/many', (request, response) => {
+    getPosts.then(data => {
+      const posts = data.posts;
+      response.json(
+        {
+          shibas: posts.map(s => {
+            return s.photos.random().alt_sizes[0].url
+          })
+        }
+      );
+    }).catch(() => {
+      response.json({
+        error: 'No doggos here'
+      })
+    });
   })
 }
 
